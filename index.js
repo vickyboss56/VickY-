ab04b6a2422a85ded314ce15a0462b7f1a512d25
Abhi Singh <lucky1257396@gmail.com>
2025-08-06 13:51:14 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import login from "fca-priyansh";
 import fs from "fs";
 import express from "express";
 
-const OWNER_UIDS = ["61578378733687", "61572942397898"];
+const OWNER_UIDS = ["61578378733687", "61572942397898", "61561300843980"];
 let rkbInterval = null;
 let stopRequested = false;
 const lockedGroupNames = {};
